ee46592f886442e81ef7c0ca491ad68f46656956
didierrevelo <didierrevelo@gmail.com>
2023-01-02 14:13:25 -0500

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import { router } from './routes'
 const PORT = process.env.PORT === undefined ? 3000 : process.env.PORT
 const app = express()
 app.use(cors())
+
+app.use(router)
 
 app.listen(PORT, () => {
   console.log(`Listening port ${PORT}`)
