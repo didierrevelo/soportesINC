45e7d953061f72d49fa710dbe2f48101bfb6973f
didierrevelo <didierrevelo@gmail.com>
2023-01-03 10:44:47 -0500

    created tables

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,24 @@ import express from 'express'
 import cors from 'cors'
 import { router } from './routes'
 import { sequelize } from './config/postgres'
+
+import './models/user'
+import './models/services'
+import './models/technicians'
+import './models/items'
+
 const PORT = process.env.PORT === undefined ? 3000 : process.env.PORT
 const app = express()
 app.use(cors())
 
 app.use(router)
 
-sequelize.authenticate()
+sequelize.sync({ force: true })
   .then(() => {
-    console.log('Conectado a la base de datos')
+    console.log('Connection has been established successfully')
   })
   .catch((err) => {
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    console.log(`No se conecto ${err}`)
+    console.error(err)
   })
 
 app.listen(PORT, () => {
